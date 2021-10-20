87a73e33e3cdfa378fd6b0e30ed20aa999de79b2
Kim Chan <chantran.24hdev@gmail.com>
2021-10-20 16:12:12 +0700

    submit email list to table

--- src/connect.php
+++ src/connect.php
@@ -1,15 +1,46 @@
 <?php
 $host = "mysql";
-$username = getenv('MYSQL_USER');
-$password = getenv('MYSQL_PASSWORD');
+$username = "root";
+$password = getenv('MYSQL_ROOT_PASSWORD');
 
 // Create connection
 $conn = new mysqli($host, $username, $password);
-
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
+} else {
+  echo "Connect to database successfully! <br>";
+
+  // create db and table
+  $sql = "CREATE TABLE contact (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(50) NOT NULL
+  )";
+
+  $conn->query("CREATE DATABASE test");
+  $conn->query("USE test");
+  $conn->query($sql);
 }
 
-echo "Connected successfully";
+
+if($_SERVER['REQUEST_METHOD'] == "POST") {
+  // insert data to `contact` table
+  $emailList = explode("\n", $_POST['emailList']);
+
+  foreach ($emailList as $email) {
+    $emailTrim = trim($email);
+    $sql = "INSERT INTO contact (email) VALUES ('$emailTrim')";
+    $conn->query($sql);
+  }
+
+  // show data from `contact` table
+  $sql = "SELECT id, email FROM contact";
+  $result = $conn->query($sql);
+
+  while($row = $result->fetch_assoc()) {
+    echo "<br>" . $row["id"]. " - Email: " . $row["email"];
+  }
+}
+
+$conn->close();
 ?>
